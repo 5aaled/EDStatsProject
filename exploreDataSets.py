76bfec5b72b35d328ad9168c348005ad13e64f95
khaled <khaledmontser17@gmail.com>
2025-10-31 15:49:33 +0100

    Updated data cleaning and visualization logic in exploreDataset.py

--- exploreDataSets.py
+++ exploreDataSets.py
@@ -1,16 +1,22 @@
 
+#  PROJET : ANALYSE DES DONNÉES MONDIALES SUR L'ÉDUCATION (EDStats)
+# Objectif : Nettoyer, fusionner et explorer plusieurs fichiers de la base de données
+#   afin d’analyser les indicateurs éducatifs par pays et par thématique.
+
 
 import numpy as np
 import pandas as pd  ,pycountry as pc , matplotlib.pyplot as plt,seaborn as sns
-from future.backports.datetime import datetime
 
-EDStatsData=pd.read_csv('Source/EdStatsData.csv')
-EDStatsFootNote =  pd.read_csv('Source/EdStatsFootNote.csv')
-EDStatsCountrySeries = pd.read_csv('Source/EdStatsCountry-Series.csv')
-EDStatsCountry = pd.read_csv('Source/EdStatsCountry.csv')
-EDStatsSeries= pd.read_csv('Source/EdStatsSeries.csv')
+# 1 :  Importation des jeux de données
+EDStatsData=pd.read_csv('Source/EdStatsData.csv')                           # Données principales : indicateurs par pays et par année
+EDStatsFootNote =  pd.read_csv('Source/EdStatsFootNote.csv')                # Notes explicatives associées aux données
+EDStatsCountrySeries = pd.read_csv('Source/EdStatsCountry-Series.csv')      # Lien entre pays et indicateurs
+EDStatsCountry = pd.read_csv('Source/EdStatsCountry.csv')                   # Métadonnées sur les pays (région, code, etc.)
+EDStatsSeries= pd.read_csv('Source/EdStatsSeries.csv')                      # Dictionnaire des indicateurs (signification, thème, etc.)
 # Explore the data
-############ 1 : EDStats Country##########
+#  Exploration et nettoyage du fichier "EDStatsCountry"
+
+# Aperçu général : dimensions, valeurs manquantes, infos descriptives
 Country_shapes = EDStatsCountry.shape
 N_of_null_values =EDStatsCountry.isnull().sum()
 Country_info =EDStatsCountry.info
@@ -21,21 +27,27 @@ Country_keys= Country_N_of_unique[Country_N_of_unique['nunique'] == Country_shap
 
 percentage_of_null_values = (N_of_null_values.sort_values(ascending=False) / Country_shapes[0]) *100
 # if null values Precnetage is bigger than 75 %  then the column is useless
-# WB-2 code  is the same as  2-alpha code  so i will delete one of them
+# Suppression des colonnes peu informatives ou redondantes
+# (par exemple : colonnes vides, codes redondants, liens inutiles)
 EDStatsCountry.drop(['Unnamed: 31','National accounts reference year','Alternative conversion factor','Other groups','2-alpha code','Table Name'],axis=1,inplace=True)
-
-valid_iso = {c.alpha_2 for c in pc.countries}
+# Vérification des codes ISO des pays pour détecter les "faux pays"
+valid_iso = {c.alpha_2 for c in pc.countries}     # Ensemble des codes ISO-2 valides
 valid_iso = list(valid_iso)
 fake_country = EDStatsCountry[~EDStatsCountry["WB-2 code"].isin(valid_iso)]
+
+# Comptage
 N_Fake_Countries = fake_country.shape[0]
 fake_country_null_values=fake_country.isnull().sum()
 fake_country_Names = fake_country['Short Name'].tolist()
 RealCountry_EDStatsCountry = EDStatsCountry[EDStatsCountry["WB-2 code"].isin(valid_iso)].drop_duplicates()
+N_of_Real_countries  =  RealCountry_EDStatsCountry.shape[0]
+print(N_of_Real_countries)
+print(len(fake_country_Names))
 
-### fill missing values in Region
+# Remplissage manuel des valeurs manquantes dans la colonne "Region"
+# (Ces pays existent réellement mais leur région est absente)
 
 Regions = RealCountry_EDStatsCountry['Region'].value_counts().index.tolist()
-
 missing_region = RealCountry_EDStatsCountry[RealCountry_EDStatsCountry["Region"].isna()]
 
 region_mapping = {"Gibraltar": "Europe & Central Asia","Nauru": "East Asia & Pacific"}
@@ -44,7 +56,9 @@ RealCountry_EDStatsCountry["Region"] = RealCountry_EDStatsCountry.apply(lambda r
 
 
 
-# 2 : EDStatsSeries : This is the dictionary of indicators — it describes what each “indicator” means and its category.
+# 3️Nettoyage et filtrage du fichier "EDStatsSeries"
+
+# On supprime les colonnes non pertinentes
 EDStatsSeries.drop(["Development relevance","Related source links","Other web links","Related indicators","License Type"],axis=1,inplace=True)
 EDStatsSeries_shapes = EDStatsSeries.shape
 EDStatsSeries_N_of_null_values =EDStatsSeries.isnull().sum()
@@ -55,11 +69,15 @@ EDStatsSeries_N_of_unique = EDStatsSeries.nunique().sort_values(ascending=False)
 EDStatsSeries_keys=EDStatsSeries_N_of_unique[EDStatsSeries_N_of_unique['nunique'] == EDStatsSeries_shapes[0]]
 
 EDStatsSeries_percentage_of_null_values =  (EDStatsSeries_N_of_null_values.sort_values(ascending=False) / EDStatsSeries_shapes[0]) *100
+# Sélection des thématiques (topics) les plus importantes pour l'analyse
 topics_to_keep = [ "Learning Outcomes", "Attainment","Education Equality","Primary", "Secondary","Tertiary","Pre-Primary","Teachers", "Expenditures", "Literacy", "Early Childhood Education", "Post-Secondary/Non-Tertiary","Population", "Education Management Information Systems (SABER)"]
 EDStatsSeries = EDStatsSeries[EDStatsSeries['Topic'].isin(topics_to_keep)].drop_duplicates()
 
 
-##### EDStatsData ###########
+#  Fusion des jeux de données pour obtenir une table consolidée
+
+# Fusion sur les clés communes : 'Country Code' et 'Indicator Code'
+# On relie les informations des pays et les thèmes des indicateurs
 
 EDStatsData= pd.merge(EDStatsData,RealCountry_EDStatsCountry['Country Code'],"inner",on='Country Code').merge(EDStatsSeries[["Series Code","Topic"]],how='inner',left_on='Indicator Code',right_on="Series Code")
 EDStatsData_columns = EDStatsData.columns.tolist()
@@ -71,28 +89,54 @@ EDStatsData_descriptions = EDStatsData.describe()
 EDStatsData_N_of_unique = EDStatsData.nunique().sort_values(ascending=False).reset_index(name='nunique')
 EDStatsData_keys=EDStatsData_N_of_unique[EDStatsData_N_of_unique['nunique'] == EDStatsData_shapes[0]]
 EDStatsData_percentage_of_null_values =  (EData_N_of_null_values.sort_values(ascending=False) / EDStatsData_shapes[0]) *100
-# drop useless columns
+# Suppression des colonnes inutiles après jointure
 EDStatsData.drop(['Unnamed: 69',"Series Code"],inplace=True,axis=1) # here i deleted also Series Code that comes from the join
 
-# 3 : EDStatsFootNote
+#  Nettoyage et préparation des autres fichiers secondaires
+# Fusion pour ne conserver que les pays et indicateurs valides
 EDStatsFootNote= pd.merge(EDStatsFootNote,RealCountry_EDStatsCountry,'inner',left_on='CountryCode',right_on='Country Code').merge(EDStatsSeries["Series Code"],how='inner',left_on="SeriesCode",right_on="Series Code").drop_duplicates()
 
-# 4 StatsCountry
+
 EDStatsCountrySeries= pd.merge(EDStatsCountrySeries,RealCountry_EDStatsCountry,'inner',left_on='CountryCode',right_on='Country Code').merge(EDStatsSeries["Series Code"],how='inner',left_on="SeriesCode",right_on="Series Code").drop_duplicates()
 
-# extract all years values to put them in a pivot table :
+#  Transformation des données et calculs de moyennes
+# Récupération de toutes les colonnes correspondant à des années
 year_cols = [c for c in EDStatsData.columns if c.isdigit()]
+
+# Calcul de la moyenne des valeurs sur toutes les années pour chaque indicateur
 EDStatsData['mean_value'] = EDStatsData[year_cols].mean(axis=1)
 
-# the average of all indicators per country :
+
+# Moyenne globale de tous les indicateurs par pays
 EDStatsData_grouped_by_country = EDStatsData.groupby("Country Name")["mean_value"].mean()
 
-# # the average of all indicators per country and topic :
+
+# Moyenne des indicateurs regroupés par pays et par thématique
 EDStatsData_groupedby_topic =EDStatsData.groupby(['Country Name','Topic'])['mean_value'].mean().reset_index(name= 'Indic_avg')
 EDStatsData_pivot_data_by_topic = EDStatsData_groupedby_topic.pivot(index= 'Country Name',columns='Topic',values='Indic_avg')
 
-# # the average of all indicators per country and indicators  :
+
+# Création de tables pivots pour visualiser les comparaisons
 EDStatsData_pivot_data_by_indicator =EDStatsData.pivot(index="Country Name" ,columns ='Indicator Code',values='mean_value')
+
+
+#  Analyse de corrélation entre thématiques et indicateurs
+plt.figure(figsize=(8, 8))
+sns.heatmap(EDStatsData_pivot_data_by_topic.corr(method="spearman"), cmap="coolwarm", center=0)
+plt.title("Correlation Between Topic")
+plt.show()
+plt.figure(figsize=(10, 8))
+sns.heatmap(EDStatsData_pivot_data_by_indicator.iloc[ : , 0 : 11].corr(), annot=True, cmap='coolwarm', center=0)
+plt.title("Correlation between indicators")
+plt.show()
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(EDStatsData_pivot_data_by_indicator.iloc[ : , 0 : 11].corr(method="spearman"), cmap="coolwarm", center=0)
+plt.title("Spearman Correlation Between Indicators")
+plt.show()
+
+
+
 
 for topic in topics_to_keep :
   df = EDStatsData[EDStatsData["Topic"] == topic]
@@ -106,18 +150,13 @@ for topic in topics_to_keep :
   plt.title(f"Top 10 Countries by {topic} (Mean Value)")
   plt.xlabel("Average Expenditure Indicator Value")
   plt.ylabel("Country")
-  #plt.show()
+  plt.show()
 
-
-plt.figure(figsize=(10, 8))
-sns.heatmap(EDStatsData_pivot_data_by_indicator.iloc[ : , 0 : 11].corr(), annot=True, cmap='coolwarm', center=0)
-plt.title("Correlation between indicators")
-#plt.show()
-
-plt.figure(figsize=(10, 8))
-sns.heatmap(EDStatsData_pivot_data_by_indicator.corr(method="spearman"), cmap="coolwarm", center=0)
-plt.title("Spearman Correlation Between Indicators")
-#plt.show()
+# Résumé :
+# - Nettoyage et fusion de 5 jeux de données du World Bank Education Stats.
+# - Filtrage des pays réels et des thématiques pertinentes.
+# - Calcul de moyennes par pays, par thématique et par indicateur.
+# - Visualisation des corrélations entre indicateurs pour détecter les redondances.
 
 
 
